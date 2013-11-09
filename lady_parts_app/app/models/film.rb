0202958ddbd94fb099bad0231987af9d36077808
Cori Allen <coriallen@gmail.com>
2013-11-09 16:46:40 -0500

    Sketch out in comments logic we want to follow when creating films in database in film model.

--- lady_parts_app/app/models/film.rb
+++ lady_parts_app/app/models/film.rb
@@ -2,9 +2,21 @@ class Film < ActiveRecord::Base
   include HTTParty
   format :json
 ### first, look in the db to see if the movie already exists
-  def self.from_
 
+  def self.from_input(imdb_id) # this should be used only by us in the beginning
+    where(imdb_id).first || get_movie_by_imdb_id
+    # if self(imdb_id).exists?
+        # get_totmato_movie_by_imdb_id
+    # else
+        # get_movie_by_imdb_id(imdb_id)
+        # get get_totmato_movie_by_imdb_id(imdb_id)
+    #  end
   end
+
+  def self.from_input(title) # this should be used when we allow user input
+    where(title).first || get_movies_by_title
+  end
+
 ### then, if it doesn't, we can get all the movie id's
 ### this will only be used the first time, and then again weekly?
   def self.get_all_movie_ids
@@ -17,7 +29,7 @@ class Film < ActiveRecord::Base
 
 ### if the search is coming from user interaction, call api once
 ### for both bechdel test and rotten tomatoes 
-  def self.get_movies_by_title(title)
+  def self.get_movies_by_title(title) # returns any movie that matches, can be more than one
     get('http://bechdeltest.com/api/v1/getMoviesByTitle', query: {title: title, output: 'json'})
   end
 
