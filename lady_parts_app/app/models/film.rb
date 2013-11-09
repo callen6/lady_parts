16aa83ae4f12475d1cc845de848ca1baf32749c7
Cori Allen <coriallen@gmail.com>
2013-11-09 17:06:08 -0500

    Add dotenv rails gem, add rottentomatoes url to gets, comment out other methods for sake of just makeing rt api calls.

--- lady_parts_app/app/models/film.rb
+++ lady_parts_app/app/models/film.rb
@@ -1,37 +1,37 @@
 class Film < ActiveRecord::Base
   include HTTParty
   format :json
-### first, look in the db to see if the movie already exists
+# ### first, look in the db to see if the movie already exists
 
-  def self.from_input(imdb_id) # this should be used only by us in the beginning
-    where(imdb_id).first || get_movie_by_imdb_id
-    # if self(imdb_id).exists?
-        # get_totmato_movie_by_imdb_id
-    # else
-        # get_movie_by_imdb_id(imdb_id)
-        # get get_totmato_movie_by_imdb_id(imdb_id)
-    #  end
-  end
+#   def self.first_or_create(imdb_id) # this should be used only by us in the beginning
+#     where(imdb_id).first || get_movie_by_imdb_id
+#     # if self(imdb_id).exists?
+#         # get_totmato_movie_by_imdb_id
+#     # else
+#         # get_movie_by_imdb_id(imdb_id)
+#         # get get_totmato_movie_by_imdb_id(imdb_id)
+#     #  end
+#   end
 
-  def self.from_input(title) # this should be used when we allow user input
-    where(title).first || get_movies_by_title
-  end
+#   def self.from_input(title) # this should be used when we allow user input
+#     where(title).first || get_movies_by_title
+#   end
 
-### then, if it doesn't, we can get all the movie id's
-### this will only be used the first time, and then again weekly?
-  def self.get_all_movie_ids
-    get('http://bechdeltest.com/api/v1/getAllMovieIds')
-  end
+# ### then, if it doesn't, we can get all the movie id's
+# ### this will only be used the first time, and then again weekly?
+#   def self.get_all_movie_ids
+#     get('http://bechdeltest.com/api/v1/getAllMovieIds')
+#   end
 
-  def self.get_movie_by_imdb_id(imdb_id)
-    get('http://bechdeltest.com/api/v1/getMovieByImdbId', query: {title: imdb_id, output: 'json'})
-  end
+#   def self.get_movie_by_imdb_id(imdb_id)
+#     get('http://bechdeltest.com/api/v1/getMovieByImdbId', query: {title: imdb_id, output: 'json'})
+#   end
 
-### if the search is coming from user interaction, call api once
-### for both bechdel test and rotten tomatoes 
-  def self.get_movies_by_title(title) # returns any movie that matches, can be more than one
-    get('http://bechdeltest.com/api/v1/getMoviesByTitle', query: {title: title, output: 'json'})
-  end
+# ### if the search is coming from user interaction, call api once
+# ### for both bechdel test and rotten tomatoes 
+#   def self.get_movies_by_title(title) # returns any movie that matches, can be more than one
+#     get('http://bechdeltest.com/api/v1/getMoviesByTitle', query: {title: title, output: 'json'})
+#   end
 
   def self.get_totmato_movie_by_imdb_id(imdb_id)
     get('', query: {title: imdb_id, output: 'json'})
@@ -40,5 +40,8 @@ class Film < ActiveRecord::Base
   def self.get_totmato_movie_by_title
      get('', query: {title: title, output: 'json'})
   end
+
+  {"link_template": "http://api.rottentomatoes.com/api/public/v1.0/movies.json?q={search-term}&page_limit={results-per-page}&page={page-number}"}
+
 
 end
