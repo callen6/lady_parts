a963213b84b40cc49d4e886006c012d819af96ec
Cori Allen <coriallen@gmail.com>
2013-11-09 15:24:19 -0500

    Create rails app.

---
+++ lady_parts_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LadyPartsApp::Application.config.secret_key_base = '14e6389f0055b04008843d8c26223e39b5aae45bb55a93c216abf00a55f4677e9237a384d8e8a1b3c72b3ef13dd31e7518c16df4e50368c33b6010b320dc3a63'
---
+++ lady_parts_app/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LadyPartsApp::Application.config.session_store :cookie_store, key: '_lady_parts_app_session'
