a241eca01afd92fb0a29b925fe3f14ed152f3f2b
Emma Spencer <emma.n.spencer@gmail.com>
2013-11-09 16:32:23 -0500

    Fixed uri calls to Bechdel API

--- lady_parts_app/app/models/film.rb
+++ lady_parts_app/app/models/film.rb
@@ -1,17 +1,17 @@
 class Film < ActiveRecord::Base
   include HTTParty
-  format: json
+  format :json
 
   def self.get_all_movie_ids
     get('http://bechdeltest.com/api/v1/getAllMovieIds')
   end
 
   def self.get_movies_by_title(title)
-    get('http://bechdeltest.com/api/v1/getMoviesByTitle', :query => {:title => title, :output => 'json'})
+    get('http://bechdeltest.com/api/v1/getMoviesByTitle?', :query => {:title => title, :output => 'json'})
   end
 
-  def self.get_movie_by_imdb_id(imdb_id)
-    get('http://bechdeltest.com/api/v1/getMovieByImdbId', :query => {:imdb_id => imdb_id, :output => 'json'})
+  def self.get_movie_by_imdb_id(imdbid)
+    get('http://bechdeltest.com/api/v1/getMovieByImdbId?', :query => {:imdb_id => imdbid, :output => 'json'})
   end
 
 end
