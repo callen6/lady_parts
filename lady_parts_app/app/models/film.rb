692b3ee80e3f155814ce2d87622484e367129ddb
Cori Allen <coriallen@gmail.com>
2013-11-09 16:21:10 -0500

    Start on rotten tomato api call methods in film model, change bechdel test methods to current ruby hash syntax.

--- lady_parts_app/app/models/film.rb
+++ lady_parts_app/app/models/film.rb
@@ -7,11 +7,19 @@ class Film < ActiveRecord::Base
   end
 
   def self.get_movies_by_title(title)
-    get('http://bechdeltest.com/api/v1/getMoviesByTitle', :query => {:title => title, :output => 'json'})
+    get('http://bechdeltest.com/api/v1/getMoviesByTitle', query: {title: title, output: 'json'})
   end
 
   def self.get_movie_by_imdb_id(imdb_id)
-    get('http://bechdeltest.com/api/v1/getMovieByImdbId', :query => {:imdb_id => imdb_id, :output => 'json'})
+    get('http://bechdeltest.com/api/v1/getMovieByImdbId', query: {title: imdb_id, output: 'json'})
+  end
+output:
+  def self.get_totmato_movie_by_imdb_id(imdb_id)
+    get('', query: {title: imdb_id, output: 'json'})
+  end
+
+  def self.get_totmato_movie_by_title
+     get('', query: {title: title, output: 'json'})
   end
 
 end
