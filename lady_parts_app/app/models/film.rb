f0867272e895af4d5e3279e92acf4f9f3d0fa666
Cori Allen <coriallen@gmail.com>
2013-11-09 18:09:46 -0500

    Fix error in get tomato movie by imdb id method query.

--- lady_parts_app/app/models/film.rb
+++ lady_parts_app/app/models/film.rb
@@ -34,7 +34,7 @@ class Film < ActiveRecord::Base
 #   end
 
   def self.get_tomato_movie_by_imdb_id(imdb_id)
-    get('api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=' + 'API_KEY' + 'q=' + imdb_id + '&page_limit=1&page=1', query: {title: imdb_id, output: 'json'})
+    get('api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=' + 'API_KEY' + 'q=' + imdb_id + '&page_limit=1&page=1', query: {imdb_id: imdb_id, output: 'json'})
   end
 # we're going to have to uri encode our queries, for example 
 # Make sure to URI encode your queries! http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=[your_api_key]&q=Toy+Story+3&page_limit=1
