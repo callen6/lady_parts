8d07d8a756ade95e972779c4d68e5267c21dc0b9
Cori Allen <coriallen@gmail.com>
2013-11-10 16:53:15 -0500

    Correct json parsing for calling a movie in RT api by imdb id added to film model.

--- lady_parts_app/app/models/film.rb
+++ lady_parts_app/app/models/film.rb
@@ -6,27 +6,19 @@ class Film < ActiveRecord::Base
 
 # first, look in the db to see if the movie already exists
 
-new_film = Film.find_or_initialize_by(imdb_id)
-new_film.update_attributes({
-  title: "movies"["title"]
-  director: "alternate_ids"["imdb"]
-  tomatoes_id: "movies"["id"]
-  })
+# new_film = Film.find_or_initialize_by(imdb_id)
+# new_film.update_attributes({
+#   title: "movies"["title"]
+#   director: "alternate_ids"["imdb"]
+#   tomatoes_id: "movies"["id"]
+#   })
 
 
 def self.create_tomato_movie_by_imdb_id(imdb_id)
   self.update(imdb_id, {
-    title: "movies"["title"]
-    director: "alternate_ids"["imdb"]
-    tomatoes_id: "movies"["id"]
-    })
-end
-
-def self.create_tomato_movie_by_title(title)
-  self.update(title, {
-    title: "movies"["title"]
-    director: "alternate_ids"["imdb"]
-    tomatoes_id: "movies"["id"]
+    film.title: film["title"]
+    film.director: film["abridged_directors"][0]["name"]
+    film.tomatoes_id: film["id"]
     })
 end
 
@@ -36,7 +28,11 @@ end
 # this will only be used the first time, and then again weekly?
 
   def self.get_all_movie_ids
-    get('http://bechdeltest.com/api/v1/getAllMovieIds')
+    all_bechdel_films = get('http://bechdeltest.com/api/v1/getAllMovieIds')
+  # this gets our information
+    all_bechdel_films.each do |film|
+      film.imdb_id: save
+      film.bechdel_id: save
   end
 
   def self.get_movie_by_imdb_id(imdb_id)
@@ -58,9 +54,12 @@ end
 
 
   def self.get_tomato_movie_by_imdb_id(imdb_id)
-    get('http://api.rottentomatoes.com/api/public/v1.0/movie_alias.json?apikey=' + ENV['API_KEY'] + '&type=imdb&id=' + imdb_id, query: {imdb_id: imdb_id, output: 'json'})
+    tomato_movie_json = get('http://api.rottentomatoes.com/api/public/v1.0/movie_alias.json?apikey=' + ENV['API_KEY'] + '&type=imdb&id=' + imdb_id, query: {imdb_id: imdb_id, output: 'json'})
+    tomato_film_by_imdb_id = JSON.parse(tomato_movie_json)
+
   end
 
+# this won't return directors
   def self.get_tomato_movie_by_title(title)
     #uri encoding in ruby http://www.ruby-doc.org/stdlib-2.0.0/libdoc/uri/rdoc/URI/Escape.html
     enc_title = URI.escape(title)
