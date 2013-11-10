8860d229d47560672b7e3c98e9110a8d629fc7cc
Cori Allen <coriallen@gmail.com>
2013-11-09 19:07:08 -0500

    Add working get tomato movie by title with working uri encoding.

--- lady_parts_app/app/models/film.rb
+++ lady_parts_app/app/models/film.rb
@@ -13,36 +13,38 @@ class Film < ActiveRecord::Base
 #     #  end
 #   end
 
-#   def self.from_input(title) # this should be used when we allow user input
-#     where(title).first || get_movies_by_title
-#   end
+  def self.from_input(title) # this should be used when we allow user input
+    where(title).first || get_movies_by_title
+  end
 
-# ### then, if it doesn't, we can get all the movie id's
-# ### this will only be used the first time, and then again weekly?
-#   def self.get_all_movie_ids
-#     get('http://bechdeltest.com/api/v1/getAllMovieIds')
-#   end
+# then, if it doesn't, we can get all the movie id's
+# this will only be used the first time, and then again weekly?
+  def self.get_all_movie_ids
+    get('http://bechdeltest.com/api/v1/getAllMovieIds')
+  end
 
-#   def self.get_movie_by_imdb_id(imdb_id)
-#     get('http://bechdeltest.com/api/v1/getMovieByImdbId', query: {title: imdb_id, output: 'json'})
-#   end
+  def self.get_movie_by_imdb_id(imdb_id)
+    get('http://bechdeltest.com/api/v1/getMovieByImdbId', query: {title: imdb_id, output: 'json'})
+  end
 
-# ### if the search is coming from user interaction, call api once
-# ### for both bechdel test and rotten tomatoes 
-#   def self.get_movies_by_title(title) # returns any movie that matches, can be more than one
-#     get('http://bechdeltest.com/api/v1/getMoviesByTitle', query: {title: title, output: 'json'})
-#   end
+# if the search is coming from user interaction, call api once
+# for both bechdel test and rotten tomatoes 
+  
+  def self.get_movies_by_title(title) # returns any movie that matches, can be more than one
+    get('http://bechdeltest.com/api/v1/getMoviesByTitle', query: {title: title, output: 'json'})
+  end
 
   def self.get_tomato_movie_by_imdb_id(imdb_id)
     get('http://api.rottentomatoes.com/api/public/v1.0/movie_alias.json?apikey=' + ENV['API_KEY'] + '&type=imdb&id=' + imdb_id, query: {imdb_id: imdb_id, output: 'json'})
   end
 
 
-# # we're going to have to uri encode our queries, for example 
-# # Make sure to URI encode your queries! http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=[your_api_key]&q=Toy+Story+3&page_limit=1
-#   def self.get_tomato_movie_by_title(title)
-#      get('api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=' + 'API_KEY' + 'q=' + title + '&page_limit=1&page=1', query: {title: title, output: 'json'})
-#   end
+# we're going to have to uri encode our queries, for example 
+# http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=[your_api_key]&q=Toy+Story+3&page_limit=1
+  def self.get_tomato_movie_by_title(title)
+    enc_title = URI.escape(title)
+    get('http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=' + ENV['API_KEY'] + '&q=' + enc_title + '&page_limit=1', query: {title: title, output: 'json'})
+  end
 
 
 end
