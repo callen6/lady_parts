d9c51ef8eae15503da1eba68d1695d302b48268f
Cori Allen <coriallen@gmail.com>
2013-11-10 21:16:13 -0500

    Create migration to add bechdel id column to films table.

--- lady_parts_app/db/schema.rb
+++ lady_parts_app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131111021148) do
+ActiveRecord::Schema.define(version: 20131111021551) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20131111021148) do
     t.integer "bechdel_rating"
     t.string  "imdb_id"
     t.integer "tomatoes_id"
+    t.integer "bechdel_id"
   end
 
 end
