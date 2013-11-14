94ff5d166043ca4288a789ae417181b9473a82d8
Emma Spencer <emma.n.spencer@gmail.com>
2013-11-14 13:47:23 -0500

    about to merge

--- app/assets/javascripts/barchart.js
+++ app/assets/javascripts/barchart.js
@@ -7,65 +7,64 @@ var Ladyparts = {
         type: 'GET',
         dataType: 'json',
         success: function(films) {
-          console.log(films);
           d3.select("#barchart").remove(),
           d3.select("#donut").remove(),
           Ladyparts.plotFilms(films);
-          Ladyparts.donutChart(films);
+          // Ladyparts.donutChart(films);
       }
   });
   },
 
 // donut chart - in progress
-  donutChart: function(films) {
-    var width = 960,
-    height = 500,
-    radius = Math.min(width, height) / 2,
+//   donutChart: function(films) {
+//     var width = 960,
+//     height = 500,
+//     radius = Math.min(width, height) / 2,
 
-    color = d3.scale.ordinal()
-              .range(["#E80C2C", "#FF7F00", "#F5F732", "#42A87A"]),
+//     color = d3.scale.ordinal()
+//               .range(["#E80C2C", "#FF7F00", "#F5F732", "#42A87A"]),
 
-    arc = d3.svg.arc()
-            .outerRadius(radius - 10)
-            .innerRadius(radius - 70),
+//     arc = d3.svg.arc()
+//             .outerRadius(radius - 10)
+//             .innerRadius(radius - 70),
 
-    pie = d3.layout.pie()
-            .sort(null)
-            .value(function(d) { return d.critics_score; }),
+//     pie = d3.layout.pie()
+//             .sort(null)
+//             .value(function(d) { return d.critics_score; }),
 
-    svg = d3.select("donut").append("svg")
-            .attr("width", width)
-            .attr("height", height)
-            .append("g")
-            .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+//     svg = d3.select("donut").append("svg")
+//             .attr("width", width)
+//             .attr("height", height)
+//             .append("g")
+//             .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-// what to do with this
-    d3.json('/films/barchart', function(json) {
-      data = json,
-      console.log(data);
-      debugger;
-      data.forEach(function(d) {
-        d.critics_score = +d.critics_score;
-  }),
+// // what to do with this
+//     d3.json('/films/barchart', function(json) {
+//       data = json,
+//       console.log(data);
+//       debugger;
+//       data.forEach(function(d) {
+//         d.critics_score = +d.critics_score;
+//   }),
 
-    g = svg.selectAll(".arc")
-      .data(pie(data))
-    .enter().append("g")
-      .attr("class", "arc");
+//     g = svg.selectAll(".arc")
+//       .data(pie(data))
+//     .enter().append("g")
+//       .attr("class", "arc");
 
-  g.append("path")
-      .attr("d", arc)
-      .style("fill", function(d) { return color(d.data.bechdel_rating); });
+//   g.append("path")
+//       .attr("d", arc)
+//       .style("fill", function(d) { return color(d.data.bechdel_rating); });
 
-  g.append("text")
-      .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
-      .attr("dy", ".35em")
-      .style("text-anchor", "middle")
-      .text(function(d) { return d.data.age; });
+//   g.append("text")
+//       .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+//       .attr("dy", ".35em")
+//       .style("text-anchor", "middle")
+//       .text(function(d) { return d.data.age; });
 
-});
+// });
 
-  },
+//   },
 
   // below is for the barchart
 
